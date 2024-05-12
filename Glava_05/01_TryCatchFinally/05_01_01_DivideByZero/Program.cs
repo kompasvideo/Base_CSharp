23d68024f0d186fdbc5bbc3073cb8f62c756ce3f
andrejbarysnikov <andrey1andrey@narod.ru>
2024-05-12 22:03:41 +0300

    05_03_02

---
+++ Glava_05/01_TryCatchFinally/05_01_01_DivideByZero/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    int x = 5;
+    int y = x / 0;
+    Console.WriteLine($"Результат {y}");
+}
+catch
+{
+    Console.WriteLine("Возникло исключение");
+}
+finally
+{
+    Console.WriteLine("Блок Finally");
+}
