dbf23f9c3609a81709cad8229802851abee67ff9
andrejbarysnikov <andrey1andrey@narod.ru>
2024-05-13 08:59:06 +0300

    05_04_01

---
+++ Glava_05/03_Exception/05_03_03_MoreExceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿try
+{
+    object obj = "you";
+    int num = (int)obj;
+    Console.WriteLine($"Результат {num}");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("DivideByZeroException");
+}
+catch (IndexOutOfRangeException)
+{
+    Console.WriteLine("IndexOutOfRangeException");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
