0499a05cd7ad4e6f03898559d7321383306f9f68
Андрей Барышников <andrey1andrey@narod.ru>
2024-07-14 02:51:14 +0300

    07_06_02

---
+++ Glava_07/06_InterfaceInGeneric/07_06_01_06_InterfaceInGeneric/Program.cs
@@ -0,0 +1,41 @@
+﻿// создаём мессенджер
+var telegram = new Messenger<Message>();
+// создаём сообщение
+var message = new Message("Hello World!");
+// отправляем сообщение
+telegram.Send(message);
+
+var telegram2 = new Messenger<PrintableMessage>();
+var message2 = new PrintableMessage("Hello World");
+telegram2.Send(message2);
+
+interface IMessage
+{
+    string Text { get; } // Текст сообщения
+}
+interface IPrintable
+{
+    void Print();
+}
+class Message: IMessage, IPrintable
+{
+    public string Text { get; }
+    public Message(string text) => Text = text;
+    public void Print() => Console.WriteLine(Text);
+}
+class Messenger<T> where T : IMessage, IPrintable
+{
+    public void Send(T message)
+    {
+        Console.WriteLine("Отправка сообщения");
+        message.Print();
+    }
+}
+
+interface IPrintableMessage : IMessage, IPrintable { }
+class PrintableMessage : IPrintableMessage
+{
+    public string Text { get; }
+    public PrintableMessage(string text) => Text = text;
+    public void Print() => Console.WriteLine(Text);
+}
---
+++ Glava_07/06_InterfaceInGeneric/07_06_02_InterfaceInGeneric/Program.cs
@@ -0,0 +1,26 @@
+﻿IUser<int> user1 = new User<int>(6789);
+Console.WriteLine(user1.Id); // 6789
+
+IUser<string> user2 = new User<string>("12345");
+Console.WriteLine(user2.Id);
+
+IUser<int> user3 = new IntUser(2345);
+Console.WriteLine(user3.Id);    //2345
+
+IntUser user4 = new IntUser(9840);
+Console.WriteLine(user4.Id);    // 9840
+
+interface IUser<T>
+{
+    T Id { get; }
+}
+class User<T> : IUser<T>
+{
+    public T Id { get; }
+    public User(T id) => Id = id;
+}
+class IntUser : IUser<int>
+{
+    public int Id { get; }
+    public IntUser(int id) => Id = id;
+}
