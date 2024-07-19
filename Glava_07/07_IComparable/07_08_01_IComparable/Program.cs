fa5b096fe19310055761c2a3e65e55b425fc83ef
Андрей Барышников <andrey1andrey@narod.ru>
2024-07-19 03:07:03 +0300

    07_08_02

---
+++ Glava_07/07_IComparable/07_08_01_IComparable/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] numbers = new int[] { 97, 45, 32, 65, 83, 23, 15 };
+Array.Sort(numbers);
+foreach(int n in numbers)
+    Console.WriteLine(n); 
+// 15 23 32 45 65 83 97
---
+++ Glava_07/07_IComparable/07_08_02_IComparable/Program.cs
@@ -0,0 +1,31 @@
+﻿//public interface IComparable
+//{
+//    int CompareTo(object? o);
+//}
+
+var tom = new Person("Tom", 37);
+var bob = new Person("Bob", 41);
+var sam = new Person("Sam", 25);
+Person[] people = {tom, bob, sam};
+Array.Sort(people);
+foreach (Person person in people)
+{
+    Console.WriteLine($"{person.Name} - {person.Age}");
+}
+
+
+class Person : IComparable
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public int CompareTo(object? o)
+    {
+        if (o is Person person) return Name.CompareTo(person.Name);
+        else throw new ArgumentException("Некорректное значение параметра");
+    }
+}
