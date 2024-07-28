e2f3cb7adc58899e40bb0146727e60451ff51143
Андрей Барышников <andrey1andrey@narod.ru>
2024-07-28 22:54:51 +0300

    08_03_05

---
+++ Glava_08/03_Index/08_03_04_Index/Program.cs
@@ -0,0 +1,8 @@
+﻿class Matrix
+{
+    int[,] numbers = new int[,] { { 1, 2, 4 }, { 2, 3, 6 }, { 3, 4, 8 } };
+    public int this[int i, int j]
+    {
+        get => numbers[i, j];
+    }
+}
---
+++ Glava_08/03_Index/08_03_05_Index/Program.cs
@@ -0,0 +1,9 @@
+﻿class Matrix
+{
+    int[,] numbers = new int[,] { { 1, 2, 4 }, { 2, 3, 6 }, { 3, 4, 8 } };
+    public int this[int i, int j]
+    {
+        get => numbers[i, j];
+        private set => numbers[i, j] = value;
+    }
+}
