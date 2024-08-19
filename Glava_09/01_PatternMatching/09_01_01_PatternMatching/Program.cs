2c436b676ec83aef4da34c046686d52fb0cae760
Андрей Барышников <andrey1andrey@narod.ru>
2024-08-19 23:36:08 +0300

    09_01_01

---
+++ Glava_09/01_PatternMatching/09_01_01_PatternMatching/Program.cs
@@ -0,0 +1,24 @@
+﻿Employee tom = new Manager();
+UseEmployee(tom);           // Manager works
+
+void UseEmployee(Employee emp)
+{
+    if (emp is Manager manager && manager.IsOnVacation == false)
+    {
+        manager.Work();
+    }
+    else
+    {
+        Console.WriteLine("Преобразование не допустимо");
+    }
+}
+
+class Employee
+{
+    public virtual void Work() => Console.WriteLine("Employee works");
+ }
+class Manager : Employee
+{
+    public override void Work() => Console.WriteLine("Manager work");
+    public bool IsOnVacation { get; set; }
+}
