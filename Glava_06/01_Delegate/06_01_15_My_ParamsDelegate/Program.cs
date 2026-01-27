fec35181873aa849247a2c22aafcb4a125187acd
Андрей Барышников <andrey1andrey@yandex.ru>
2026-01-27 03:54:59 +0300

    1

---
+++ Glava_06/01_Delegate/06_01_15_My_ParamsDelegate/Program.cs
@@ -0,0 +1,14 @@
+﻿DoOperation(5, 4, Add);
+DoOperation(5, 4, Subtract);
+DoOperation(5, 4, Multiply);
+
+void DoOperation(int a, int b, Operation op)
+{
+    Console.WriteLine(op(a, b));
+}
+
+int Add(int x, int y) => x + y;
+int Subtract(int x, int y) => x - y;
+int Multiply(int x, int y) => x * y;
+
+delegate int Operation(int x, int y);
