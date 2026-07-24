5a432f2a1233bdff0847b35a257893c82037c04a
andrejbarysnikov <andrey1andrey@yandex.ru>
2026-07-24 04:16:27 +0300

    10-2-05

--- Glava_10/02_LinkedList/10_2_03_LinkedListNode/Program.cs
+++ Glava_10/02_LinkedList/10_2_03_LinkedListNode/Program.cs
@@ -1,2 +1,19 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿LinkedList<string> people = new LinkedList<string>(new[] {"Tom", "Sam", "Bob"});
+
+// от начала до конца списка
+var currentNode = people.First;
+while (currentNode != null)
+{
+    Console.WriteLine(currentNode.Value);
+    currentNode = currentNode.Next;
+}
+
+Console.WriteLine();
+
+// с конца до начала списка
+currentNode = people.Last;
+while (currentNode != null)
+{
+    Console.WriteLine(currentNode.Value);
+    currentNode = currentNode.Previous;
+}
---
+++ Glava_10/02_LinkedList/10_2_04_AddFirst/Program.cs
@@ -0,0 +1,10 @@
+﻿var people = new LinkedList<string>();
+people.AddLast("Tom");      // вставляем узел со значением Tom на последнее место
+                            //так как в списке нет узлов, то последнее будет также и первым
+people.AddFirst("Bob");     // вставляем узел со значением Bob на первое место
+
+// вставляем после первого узла новый узел со значением Mike
+if(people.First != null) people.AddAfter(people.First, "Mike");
+
+// теперь у нас список имеет следующую последовательность: Bob Mike Tom
+foreach(var person in people) Console.WriteLine(person);
