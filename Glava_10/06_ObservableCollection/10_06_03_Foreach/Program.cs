6d051fe89fa84c3d111dc8d1675bccbe1a7f4446
andrejbarysnikov <andrey1andrey@yandex.ru>
2026-08-02 14:59:58 +0300

    10-06-05

---
+++ Glava_10/06_ObservableCollection/10_06_03_Foreach/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+var people = new ObservableCollection<string>()
+{
+    "Tom", "Bob", "Sam"
+};
+foreach (var person in people)
+{
+    Console.WriteLine(person);
+}
+
+Console.WriteLine();
+for (int i = 0; i < people.Count; i++)
+{
+    Console.WriteLine(people[i]);
+}
