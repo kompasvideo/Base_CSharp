2d2ca853cbec5cb29791582b5c514055dcdd7905
andrejbarysnikov <andrey1andrey@yandex.ru>
2026-08-04 23:36:10 +0300

    10-07-03

---
+++ Glava_10/07_IEnumerable/10_07_01_IEnumerable/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+string[] people = { "Tom", "Sam", "Bob" };
+IEnumerator peopleEnumerator = people.GetEnumerator();
+while (peopleEnumerator.MoveNext())
+{
+    string item = (string)peopleEnumerator.Current;
+    Console.WriteLine(item);
+}
---
+++ Glava_10/07_IEnumerable/10_07_02_IEnumerable/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+Week week = new Week();
+foreach (var item in week)
+{
+    Console.WriteLine(item);
+}
+
+class Week : IEnumerable
+{
+    string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+    public IEnumerator GetEnumerator() => days.GetEnumerator();
+}
